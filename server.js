04832bd73c9f5a93b21332da898d0efc4d74484b
Kosta Rajic <kosta.rajic.2@gmail.com>
2025-01-06 21:01:18 +0100

    zxcvb

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors');
+
+const app = express();
+const corsOptions = {
+    origin: ['portfolio-ecru-phi-55.vercel.app'],
+    methods: ['POST', 'GET'],
+    credentials: true
+  }
+  
+  app.use(cors(corsOptions));
+  
+
+app.get('/', (req, res) => {
+    res.status(200).send('SERVER IS RUNNING')
+  })
+
+  app.listen(4000, () => {
+    console.log(`Server is running on http://localhost:4000`);
+  });
